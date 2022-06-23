40ed3be70eed09760ed8d51a2d12a64b41b6fafa
mica-galvan <daianamicaelagalvan@gmail.com>
2022-06-22 22:50:12 -0300

    Se entrega 1er desafio Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './modulos/navbar/navbar.component';
+import { ToolbarComponent } from './modulos/toolbar/toolbar.component';
+import { MainComponent } from './modulos/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
